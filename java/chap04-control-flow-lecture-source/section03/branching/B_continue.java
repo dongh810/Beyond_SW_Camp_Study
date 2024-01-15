a36341084f6b9e0a4c28c502163c4a2a390cc182
dongh810 <baek0810@naver.com>
2024-01-15 18:03:20 +0900

    24.01.15 branching 실습코드

---
+++ java/chap04-control-flow-lecture-source/section03/branching/B_continue.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.section03.branching;
+
+public class B_continue {
+    public void testSimpleContinueStatement() {
+
+        /* 수업목표. continue문 사용에 대한 흐름을 이해하고, 적용될 수 있다. */
+        /* 필기. continue문 또한 반복문 안에서 사용한다. continue구문 이후 반복할 내용을 건너뛰고 다음 스텝으로 넘어간다. */
+
+        /* 설명. 1부터 100 사이의 4의 배수이면서 5의 배수인 값 출력(4와 5의 공배수) */
+        for (int i =1; i<=100; i++) {
+
+            // if(4와 5의 공배수가 아니면)
+            // 아래출력을 건너뛴다.
+            if(!(i%4 == 0 && i%5==0)){
+                continue;
+            }
+
+            System.out.println("i = " + i);
+        }
+    }
+}
