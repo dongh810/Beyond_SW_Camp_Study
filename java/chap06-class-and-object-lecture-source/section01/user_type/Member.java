1170624d37feec79f9a614666072ca2924fe8896
dongh810 <baek0810@naver.com>
2024-01-16 18:13:29 +0900

    24.01.16 user_type 실습코드

---
+++ java/chap06-class-and-object-lecture-source/section01/user_type/Member.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section01.user_type;
+
+public class Member {
+
+    /* 설명. 여러가지 타입의 값을 하나의 타입(Member 클래스)으로 묶을 수 있다. */
+    String id;
+    String pwd;
+    String name;
+    int age;
+    char gender;
+    String[] hobby;
+}
