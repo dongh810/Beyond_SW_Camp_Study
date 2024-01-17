fc0c029d63ef12076f6cab7a91dba490024e0c0b
dongh810 <baek0810@naver.com>
2024-01-17 17:50:41 +0900

    24.01.17 initblock 실습코드

---
+++ java/chap06-class-and-object-lecture-source/section07/initblock/Application.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section07.initblock;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* 수업목표. 초기화 블럭의 동작 순서를 이해하고 이를 활용하여 인스턴스를 생성할 수 있다. */
+        Product product = new Product();
+        System.out.println(product);
+       // Product product2 = new Product("샤오미");
+
+    }
+}
