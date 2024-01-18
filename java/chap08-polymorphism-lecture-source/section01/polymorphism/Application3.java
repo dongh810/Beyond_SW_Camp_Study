8cad3fc753906731291265608de2a78344f80ed6
dongh810 <baek0810@naver.com>
2024-01-18 17:55:01 +0900

    24.01.18 polymorphism 실습코드

---
+++ java/chap08-polymorphism-lecture-source/section01/polymorphism/Application3.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section01.polymorphism;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+        /* 수업목표. 다형성을 적용하여 매개변수에 활용할 수 있음을 이해할 수 있다. */
+        Application3 app3 = new Application3();
+
+        Animal animal1 = new Rabbit();
+        Animal animal2 = new Tiger();
+
+        app3.feed(animal1);
+        app3.feed(animal2);
+    }
+
+    private void feed(Animal animal1) {
+        animal1.eat();
+    }
+
+
+}
+
+
