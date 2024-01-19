91a94da89c25e338f298ba1c205d6bcbea461ebb
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-01-19 16:23:25 +0900

    24.01.19 string 실습코드

---
+++ java/chap09-api-lecture-source/section02/string/Application4.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section02.string;
+
+public class Application4 {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요 \n 저는 홍길동입니다.");
+        System.out.println("안녕하세요 \t 저는 홍길동입니다.");
+
+        System.out.println("안녕하세요 저는 '홍길동'입니다.");
+        System.out.println('\'');
+        System.out.println("안녕하세요 저는 \"홍길동\"입니다.");
+
+        System.out.println("역슬래쉬(\\)입니다.");
+
+        System.out.printf("원주율은 %.2f입니다. 우린 %d로 하죠", 3.141592,3);
+
+    }
+}
