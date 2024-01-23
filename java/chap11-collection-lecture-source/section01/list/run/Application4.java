c647236e922cf29cffb8c7d787f694603b61f5fb
dongh810 <baek0810@naver.com>
2024-01-23 17:52:48 +0900

    24.01.23 list 실습코드

---
+++ java/chap11-collection-lecture-source/section01/list/run/Application4.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.section01.list.run;
+
+import java.util.Stack;
+
+public class Application4 {
+    public static void main(String[] args) {
+
+        /* 수업목표. stack에 대해 이해하고 사용할 수 있다. */
+        /* 필기.
+         *  후입선출(LIFO) 도는 선입후출(FILO) 구조의 자료구조이다.
+         *  push(), pop(), peek() 등의 메소드를 활용하여 자료를 처리할 수 있다.
+         *  Vector를 상속한 클래스이다.
+        * */
+        Stack<Integer> integerStack = new Stack<>();
+
+        /* 설명. Stack 인스턴스 생성 */
+        integerStack.push(1);
+        integerStack.push(2);
+        integerStack.push(3);
+        integerStack.push(4);
+        integerStack.push(5);
+
+        System.out.println(integerStack);
+
+        /* 필기.
+         *  peek(): 해당 스택의 최상단(top)에 있는 요소 반환
+         *  search(): 해당 요소와 일치하는 요소의 위치를 최상단에서부터 몇 번째인지 반환
+         *  pop(): 해당 스택의 최상단에 있는 요소 반환 후 제거
+        * */
+        System.out.println("peak(): " + integerStack.peek());
+        System.out.println(integerStack);
+
+        System.out.println("search(): " + integerStack.search(2));
+
+        System.out.println("pop(): " + integerStack.pop());
+        System.out.println("pop(): " + integerStack.pop());
+        System.out.println("pop(): " + integerStack.pop());
+        System.out.println("pop(): " + integerStack.pop());
+        System.out.println("pop(): " + integerStack.pop());
+        System.out.println(integerStack);
+    }
+}
