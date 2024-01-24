9c11924243493f97510786b0f0b8c45d6a312eef
dongh810 <baek0810@naver.com>
2024-01-24 16:24:00 +0900

    24.01.24 exception 실습코드

---
+++ java/chap12-exception-lecture-source/section01/exception/ExceptionTest.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section01.exception;
+
+public class ExceptionTest {
+
+    public void checkEnoughMoney(int price, int money) throws Exception {
+        System.out.println("가지고 계신 돈은 " + money + "원 입니다.");
+
+        if (money >= price) {
+            System.out.println(price + "원 상품을 구입하기 위한 금액이 충분합니다.");
+        } else {            // 예외 상황
+            throw new Exception("호주머니 사정이 딱하시군요!");
+        }
+    }
+}
