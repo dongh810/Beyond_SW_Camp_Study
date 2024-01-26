c06233ab8c528dd2565b535e87c95cfa947afb1f
dongh810 <baek0810@naver.com>
2024-01-26 19:20:36 +0900

    24.01.26 lamda_intro 실습코드

---
+++ java/chap15-lambda-lecture-source/section01/intro/OuterCalculator.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.section01.intro;
+
+public interface OuterCalculator {
+    @FunctionalInterface
+    public interface Sum{
+        int sumTwoNumber(int a, int b);
+    }
+
+    @FunctionalInterface
+    public interface Minus{
+        int minusTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Multiple{
+        int multiplyTwoNumber(int a, int b);
+    }
+    @FunctionalInterface
+    public interface Divide{
+        int divideTwoNumber(int a, int b);
+    }
+
+}
