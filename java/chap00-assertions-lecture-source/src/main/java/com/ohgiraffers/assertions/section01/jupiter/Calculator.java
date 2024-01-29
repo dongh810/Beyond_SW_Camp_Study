96c693b6733dbb548cfd4c24cb01af308d7eadda
dongh810 <baek0810@naver.com>
2024-01-29 17:49:28 +0900

    24.01.29 test 실습코드

---
+++ java/chap00-assertions-lecture-source/src/main/java/com/ohgiraffers/assertions/section01/jupiter/Calculator.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.assertions.section01.jupiter;
+
+public class Calculator {
+
+
+    public int plusTwoNumbers(int firstNum, int secondNum) {
+        return firstNum + secondNum;
+    }
+}
