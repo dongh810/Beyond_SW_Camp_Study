86ada4a10612c610e02d00069228be15baac92bb
dongh810 <baek0810@naver.com>
2024-02-07 17:55:58 +0900

    24.02.07 forward 실습코드

---
+++ servlet/chap07-forward-lecture-source/src/main/java/com/ohgiraffers/section01/forward/PrintLoginSucessServlet.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.section01.forward;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/print")
+public class PrintLoginSucessServlet extends HttpServlet {
+    public PrintLoginSucessServlet() {
+    }
+
+    /* 설명. 이전 서블릿이 doPost로 처리하다가 포워딩 되면 doPost로 받아야 한다.(doGet은 doGet으로 처리) */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("==== 포워딩 된 서블릿에서 넘겨받은 request객체에 담긴 값 확인 ====");
+        System.out.println("이름: " + req.getAttribute("userName"));
+        System.out.println("아이디: " + req.getParameter("userId"));
+        System.out.println("패스워드: " + req.getParameter("password"));
+
+        String userName = (String)req.getAttribute("userName");
+        StringBuilder responseText = new StringBuilder();
+        responseText.append("<h3 align=\"center\">\n")
+                .append(userName)
+                .append("님 환영합니다.</h3>");
+
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.print(responseText);
+        out.flush();
+        out.close();
+    }
+}
