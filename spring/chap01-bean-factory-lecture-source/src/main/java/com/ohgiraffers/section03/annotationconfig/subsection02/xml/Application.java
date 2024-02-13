7b738bbce4fc1422013c6115ae1df33cd393cce5
dongh810 <baek0810@naver.com>
2024-02-13 17:52:38 +0900

    24.02.13 anootationconfig 실습코드

---
+++ spring/chap01-bean-factory-lecture-source/src/main/java/com/ohgiraffers/section03/annotationconfig/subsection02/xml/Application.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.section03.annotationconfig.subsection02.xml;
+
+import com.ohgiraffers.common.MemberDAO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context
+                = new GenericXmlApplicationContext("section03/annotationconfig/subsection02/xml/spring-context.xml");
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        for( String beanName : beanNames) {
+            System.out.println("beanName = " + beanName);
+        }
+
+    }
+}
