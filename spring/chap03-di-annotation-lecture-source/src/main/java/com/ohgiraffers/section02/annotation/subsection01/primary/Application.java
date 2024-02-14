078f3bb65855a4edfbe7aa2e3e8c4238f9d8ca17
dongh810 <baek0810@naver.com>
2024-02-14 18:23:33 +0900

    24.02.14 annotation-primary 실습코드

---
+++ spring/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/annotation/subsection01/primary/Application.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.section02.annotation.subsection01.primary;
+
+import com.ohgiraffers.section02.annotation.common.Pokemon;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context
+                = new AnnotationConfigApplicationContext("com.ohgiraffers.section02");
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        for(String beanName : beanNames) {
+            System.out.println("beanName = " + beanName);
+        }
+
+        PokemonService pokemonService = context.getBean("pokemonServicePrimary",PokemonService.class);
+
+        pokemonService.pokemonAttack();
+    }
+
+
+
+}
