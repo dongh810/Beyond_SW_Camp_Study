ecb7fb17a9a1f1fb38cb35bdaddfca7e4fd5ecbc
dongh810 <baek0810@naver.com>
2024-02-14 18:27:09 +0900

    24.02.14 aop 실습코드

---
+++ spring/chap05-aop-lecture-source/src/main/java/com/ohgiraffers/section01/aop/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section01.aop;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+    private Long id;
+    private String name;
+}
