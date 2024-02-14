ff9532aa5eb6a6e9c8f007c865e4a89ad0816ac7
dongh810 <baek0810@naver.com>
2024-02-14 18:26:33 +0900

    24.02.14 properties 실습코드

---
+++ spring/chap04-bean-scope-lecture-source/src/main/java/com/ohgiraffers/section03/properties/subsection01/properties/Application.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.section03.properties.subsection01.properties;
+
+import com.ohgiraffers.common.Beverage;
+import com.ohgiraffers.common.Bread;
+import com.ohgiraffers.common.Product;
+import com.ohgiraffers.common.ShoppingCart;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ContextConfiguration.class);
+
+        String[] beanNames = context.getBeanDefinitionNames();
+        for(String beanName : beanNames) {
+            System.out.println("beanName = " + beanName);
+        }
+
+
+        Product carpBread = context.getBean("carpBread", Bread.class);
+        Product milk = context.getBean("milk", Beverage.class);
+        Product water = context.getBean("water", Beverage.class);
+
+        ShoppingCart cart1 = context.getBean("cart", ShoppingCart.class);
+        cart1.addItem(carpBread);
+        cart1.addItem(milk);
+
+        System.out.println("cart1에 담긴 물품: " + cart1.getItems());
+
+        ShoppingCart cart2 = context.getBean("cart", ShoppingCart.class);
+        cart2.addItem(water);
+
+        System.out.println("cart2에 담긴 물품: " + cart2.getItems());
+
+    }
+
+}
---
+++ spring/chap04-bean-scope-lecture-source/src/main/java/com/ohgiraffers/section03/properties/subsection01/properties/ContextConfiguration.java
@@ -0,0 +1,67 @@
+package com.ohgiraffers.section03.properties.subsection01.properties;
+
+import com.ohgiraffers.common.Beverage;
+import com.ohgiraffers.common.Bread;
+import com.ohgiraffers.common.Product;
+import com.ohgiraffers.common.ShoppingCart;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@PropertySource("section03/properties/subsection01/properties/product-info.properties")
+public class ContextConfiguration {
+
+    /* value를 통해 propertysource에 있는 값을 대입해준다.
+    그래서 깃허브에는 이렇게만 올리고 properties파일만 안올리면
+    값은 비밀로 할수있음. */
+    @Value("${bread.carpbread.name}")
+    private String carpBreadName;
+
+    @Value("${bread.carpbread.price}")
+    private int carpBreadPrice;
+
+    @Value("${beverage.milk.name}")
+    private String milkName;
+
+    @Value("${beverage.milk.price}")
+    private int milkPrice;
+
+    @Value("${beverage.milk.capacity}")
+    private int milkCapacity;
+
+//    @Value("${beverage.water.name}")
+//    private String waterName;
+//
+//    @Value("${beverage.water.price}")
+//    private int waterPrice;
+//
+//    @Value("${beverage.water.capacity}")
+//    private int waterCapacity;
+
+    @Bean
+    public Product carpBread() {
+        return new Bread(carpBreadName, carpBreadPrice, new java.util.Date());
+    }
+
+    @Bean
+    public Product milk() {
+        return new Beverage(milkName, milkPrice, milkCapacity);
+    }
+
+    @Bean
+    public Product water(@Value("${beverage.water.name}") String waterName,
+                         @Value("${beverage.water.price}") int waterPrice,
+                         @Value("${beverage.water.capacity}") int waterCapacity) {
+        return new Beverage(waterName, waterPrice, waterCapacity);
+    }
+
+    @Bean
+    @Scope("prototype")
+    public ShoppingCart cart() {
+        return new ShoppingCart();
+    }
+
+}
