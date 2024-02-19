6f95072175276264ddc08db380c31a96233cce01
dongh810 <baek0810@naver.com>
2024-02-19 18:03:08 +0900

    24.02.19 setting 파일

---
+++ springboot/chap06-interceptor-lecture-source/src/test/java/com/ohgiraffers/chap06interceptorlecturesource/Chap06InterceptorLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.chap06interceptorlecturesource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap06InterceptorLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
