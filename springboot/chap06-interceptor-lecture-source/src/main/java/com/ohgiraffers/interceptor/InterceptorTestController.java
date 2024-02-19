d760c1678f281154b4bba7f4f0ad77c496ae8252
dongh810 <baek0810@naver.com>
2024-02-19 18:05:31 +0900

    24.02.19 interceptor 실습코드

---
+++ springboot/chap06-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/InterceptorTestController.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InterceptorTestController {
+
+    @GetMapping("stopwatch")
+    public String handlerMethod() throws InterruptedException {
+        System.out.println("핸들러 메소드 호출함");
+
+        Thread.sleep(1000);
+
+        return "result";
+    }
+}
---
+++ springboot/chap06-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/MainController.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String defaultlocation() {
+        return "main";
+    }
+
+    @RequestMapping("/main")
+    public void main() {}
+}
+
