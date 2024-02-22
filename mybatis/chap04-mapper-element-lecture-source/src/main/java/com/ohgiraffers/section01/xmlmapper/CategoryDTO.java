3ac5e23917126334554ddcdff553377a61772d7a
dongh810 <baek0810@naver.com>
2024-02-22 16:45:24 +0900

    24.02.22 xmlmapper 실습코드

---
+++ mybatis/chap04-mapper-element-lecture-source/src/main/java/com/ohgiraffers/section01/xmlmapper/CategoryDTO.java
@@ -0,0 +1,49 @@
+package com.ohgiraffers.section01.xmlmapper;
+
+public class CategoryDTO {
+    private int categoryCode;
+    private String categoryName;
+    private Integer refCategoryCode;
+
+    public CategoryDTO(int categoryCode, String categoryName, Integer refCategoryCode) {
+        this.categoryCode = categoryCode;
+        this.categoryName = categoryName;
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    public CategoryDTO() {
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getRefCategoryCode() {
+        return refCategoryCode;
+    }
+
+    public void setRefCategoryCode(Integer refCategoryCode) {
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryDTO{" +
+                "categoryCode=" + categoryCode +
+                ", categoryName='" + categoryName + '\'' +
+                ", refCategoryCode=" + refCategoryCode +
+                '}';
+    }
+}
