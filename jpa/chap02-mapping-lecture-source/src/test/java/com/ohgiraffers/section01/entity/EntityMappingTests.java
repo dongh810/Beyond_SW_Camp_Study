ebd45bdb8e3fdb0145557ac294766b48b2182b74
dongh810 <baek0810@naver.com>
2024-02-26 18:08:14 +0900

    24.02.26 mapping 실습코드

---
+++ jpa/chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/entity/EntityMappingTests.java
@@ -0,0 +1,72 @@
+package com.ohgiraffers.section01.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EntityMappingTests {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @Test
+    public void 테이블_만들기_테스트() {
+
+        //given
+        Member member = new Member();
+        member.setMemberNo(1);
+        member.setMemberId("user01");
+        member.setMemberPwd("pass01");
+        member.setNickname("홍길동");
+        member.setPhone("010-1234-1234");
+        member.setEmail("gonh@email.com");
+        member.setAddress("서울시 서초구");
+        member.setEnrollDate(new java.util.Date());
+        member.setMemberRole("ROLE_MEMBER");
+        member.setStatus("Y");
+
+        //when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        entityManager.persist(member);
+
+        //then
+        Member foundMember = entityManager.find(Member.class, 1);
+        foundMember.setNickname("동해번쩍");
+
+        entityTransaction.commit();
+        assertEquals(member, foundMember);
+
+    }
+}
+
+
+
+
