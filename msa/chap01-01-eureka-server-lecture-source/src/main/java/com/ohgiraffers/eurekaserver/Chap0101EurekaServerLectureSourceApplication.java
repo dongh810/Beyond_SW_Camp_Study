abdf61b1790af6ed8d4de045077b9d691dcadbbd
dongh810 <baek0810@naver.com>
2024-03-04 16:44:07 +0900

    24.03.04 eureka server 실습코드

---
+++ msa/chap01-01-eureka-server-lecture-source/src/main/java/com/ohgiraffers/eurekaserver/Chap0101EurekaServerLectureSourceApplication.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+
+
+/* 설명. 유레카 서버의 자격을 등록(서비스 디스커버리로써 지금 프로젝트를 기동) */
+@EnableEurekaServer
+public class Chap0101EurekaServerLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap0101EurekaServerLectureSourceApplication.class, args);
+    }
+
+}
