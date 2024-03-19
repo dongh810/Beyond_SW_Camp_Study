14eab02cdc8a9f38b2c5e3c704b59297946d9ef3
dongh810 <baek0810@naver.com>
2024-03-19 16:11:18 +0900

    24.03.19 spread-syntax 실습코드

---
+++ javascript/02_javascript_es6/03_spread-syntax/02_array-and-object-copy/01_array-and-object-copy.js
@@ -0,0 +1,22 @@
+/* 01. array-and-object-copy(스프레드 문법을 이용한 배열 및 개체 복사) */
+
+/* 1. 배열 복사 */
+let arr = [10,30,20];
+let arrCopy = [...arr]; //스프레드 문법을 활용한 깊은 복사
+
+console.log(arr);
+console.log(arrCopy);
+console.log(arr === arrCopy);
+
+/* 2. 객체 복사 */
+let obj = {name: '홍길동', age:20};
+let objCopy = {...obj}; // 사본 만들기
+
+
+/* 추가적으로 나머지 연산자 개념으로 활용도 가능하다. */
+// let age =30;
+// let objCopy = {...obj,age};
+
+console.log(obj);
+console.log(objCopy);
+console.log(obj === objCopy);
