d209ab039b09984573f647bd2d94e23eaf01578f
dongh810 <baek0810@naver.com>
2024-03-25 16:17:12 +0900

    2024.03.25 slot 실습코드

---
+++ vue/06_slot/src/components/01_component/MyComponent.vue
@@ -0,0 +1,23 @@
+<!-- vbase-3 setup -->
+<template>
+    <div>
+        <h1>MyComponenet</h1>
+    </div>
+</template>
+
+<script>
+    import {onMounted} from 'vue';
+
+    export default {
+        setup() {
+            onMounted(async() => {
+                console.log("Helloo Vue!");
+            })
+        }
+    }
+    
+</script>
+
+<style scoped>
+
+</style>
